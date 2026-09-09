6700c9af300bf624a326b812b05dde4e27686983
unknown <ishwargurung011@email.com>
2026-09-09 10:40:00 +0545

    Client refactored

--- components/Client/ClientList/type.ts
+++ components/Client/ClientList/type.ts
@@ -1,65 +1,13 @@
-export type ClientStaffRecord = {
-  _id?: string;
-  staffId?: number | string;
-  id?: number | string;
-  name: string;
-  email?: string;
-  phone?: string;
-  location?: string;
-};
+import type { ClientPageData, ClientRecord, ClientStaffRecord } from "../client-query";
 
-export type ClientRecord = {
-  _id?: string;
-  clientId: number;
-  fullName: string;
-  dateOfBirth?: string;
-  gender?: string;
-  phone?: string;
-  email?: string;
-  address?: string;
-  nationality?: string;
-  passportNumber?: string;
-  passportExpiryDate?: string;
-  visaType?: string;
-  statusOfResidence?: string;
-  lastQualification?: string;
-  japaneseLanguageLevel?: string;
-  schoolName?: string;
-  course?: string;
-  intake?: string;
-  jobCategory?: string;
-  jobTitle?: string;
-  companyName?: string;
-  workLocation?: string;
-  sponsorName?: string;
-  sponsorRelationship?: string;
-  sponsorStatusOfResidence?: string;
-  coeStatus?: string;
-  visaStatus?: string;
-  clientStatus?: string;
-  remarks?: string;
-  clientImage?: string;
-  cv?: string;
-  assignedStaff?: ClientStaffRecord | string | null;
-  assignedStaffId?: number | string | null;
-  assignedStaffName?: string;
-};
-
-export type ClientApiResponse = Omit<
+export type {
+  ClientApiResponse,
+  ClientListApiResponse,
   ClientRecord,
-  "clientId" | "assignedStaffId" | "assignedStaffName"
-> & {
-  clientId?: number | string;
-};
+  ClientStaffRecord,
+} from "../client-query";
 
-export type ClientListApiResponse<T> = {
-  data?: T[];
-};
-
-export type ClientListData = {
-  staffs: ClientStaffRecord[];
-  clients: ClientRecord[];
-};
+export type ClientListData = ClientPageData;
 
 export type ClientListViewState = {
   sidebarCollapsed: boolean;
--- components/Client/EditClientPage/type.ts
+++ components/Client/EditClientPage/type.ts
@@ -1,60 +1,11 @@
-export type ClientStaffRecord = {
-  _id?: string;
-  staffId?: number | string;
-  id?: number | string;
-  name: string;
-  email?: string;
-  phone?: string;
-  location?: string;
-};
+import type { ClientRecord } from "../client-query";
 
-export type ClientRecord = {
-  _id?: string;
-  clientId: number;
-  fullName: string;
-  dateOfBirth?: string;
-  gender?: string;
-  phone?: string;
-  email?: string;
-  address?: string;
-  nationality?: string;
-  passportNumber?: string;
-  passportExpiryDate?: string;
-  visaType?: string;
-  statusOfResidence?: string;
-  lastQualification?: string;
-  japaneseLanguageLevel?: string;
-  schoolName?: string;
-  course?: string;
-  intake?: string;
-  jobCategory?: string;
-  jobTitle?: string;
-  companyName?: string;
-  workLocation?: string;
-  sponsorName?: string;
-  sponsorRelationship?: string;
-  sponsorStatusOfResidence?: string;
-  coeStatus?: string;
-  visaStatus?: string;
-  clientStatus?: string;
-  remarks?: string;
-  clientImage?: string;
-  cv?: string;
-  assignedStaff?: ClientStaffRecord | string | null;
-  assignedStaffId?: number | string | null;
-  assignedStaffName?: string;
-};
-
-export type ClientApiResponse = Omit<
+export type {
+  ClientApiResponse,
+  ClientListApiResponse,
   ClientRecord,
-  "clientId" | "assignedStaffId" | "assignedStaffName"
-> & {
-  clientId?: number | string;
-};
-
-export type ClientListApiResponse<T> = {
-  data?: T[];
-};
+  ClientStaffRecord,
+} from "../client-query";
 
 export type EditClientViewState = {
   clientId: number;
