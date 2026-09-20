97064b54890996aeef3827d03e4b1f5be26386dc
unknown <ishwargurung011@email.com>
2026-09-20 16:45:23 +0545

    removed console

--- components/Client/CreateClient/hook.ts
+++ components/Client/CreateClient/hook.ts
@@ -362,7 +362,6 @@ export const useCreateClientHook = () => {
       // =================================================
       // CREATE
       // =================================================
-      console.log("formData", formData);
 
       await createClient(formData);
 
